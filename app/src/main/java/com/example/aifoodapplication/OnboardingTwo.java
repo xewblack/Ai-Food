cdc27f253525532b4fbf2079c2213da29fec1da6
Илья Ерохин <ilaerohin@MacBook-Air-Ila.local>
2025-06-13 12:49:39 +0600

    4 экрана

--- app/src/main/java/com/example/aifoodapplication/OnboardingTwo.java
+++ app/src/main/java/com/example/aifoodapplication/OnboardingTwo.java
@@ -1,6 +1,8 @@
 package com.example.aifoodapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class OnboardingTwo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_onboarding_two);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void Skip2(View view) {
+        Intent intent = new Intent(OnboardingTwo.this, OnboardingThree.class);
+        startActivity(intent);
+    }
+
+    public void Next2(View view) {
+        Intent intent = new Intent(OnboardingTwo.this, OnboardingThree.class);
+        startActivity(intent);
+    }
+
 }
